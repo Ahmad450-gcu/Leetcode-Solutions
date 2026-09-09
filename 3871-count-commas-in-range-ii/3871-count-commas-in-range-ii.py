7c38ed82c54bd099ff498550387ff64b60b68613
Ahmad450-gcu <ahmadmaqbool4500@gmail.com>
2026-09-09 14:58:04 +0500

    Time: 0 ms (100%), Space: 19.3 MB (51.7%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -1,15 +1,22 @@
 class Solution:
     def countCommas(self, num: int) -> int:
-        strs = str(num)
-        n = len(strs)
-        if n < 4:
-            return 0
-        if n < 7:
-            return num - 999
-        if n < 10:
-            return (num - 999999) + (num - 999)
-        if n < 13:
-            return (num - 999999999) + (num - 999999) + (num - 999)
-        if n < 16:
-            return (num - 999999999999) + (num - 999999999) + (num - 999999) + (num - 999) 
-        return (num - 999999999999) + (num - 999999999) + (num - 999999) + (num - 999) + 1 
+        # strs = str(num)
+        # n = len(strs)
+        # if n < 4:
+        #     return 0
+        # if n < 7:
+        #     return num - 999
+        # if n < 10:
+        #     return (num - 999999) + (num - 999)
+        # if n < 13:
+        #     return (num - 999999999) + (num - 999999) + (num - 999)
+        # if n < 16:
+        #     return (num - 999999999999) + (num - 999999999) + (num - 999999) + (num - 999) 
+        # return (num - 999999999999) + (num - 999999999) + (num - 999999) + (num - 999) + 1 
+
+        ans = 0
+        factor = 1000
+        while num >= factor:
+            ans += (num - factor) + 1
+            factor *= 1000
+        return ans
