1607984657450db9e37b0feb4ca8b790e85d8c0b
Ahmad450-gcu <ahmadmaqbool4500@gmail.com>
2026-09-20 13:02:53 +0500

    Time: 15 ms (24.72%), Space: 12.5 MB (0.74%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        output = -1
+        output = 0
         for i, char in enumerate(s):
             output += (i + 1) *  (27 - (ord(char) - 96)) 
-        return output + 1
+        return output 
